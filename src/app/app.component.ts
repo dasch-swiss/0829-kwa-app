842d7bd6dc2798f5605a73f12c373b292b283f46
flaurens <flavie.laurens@unibas.ch>
2020-05-13 17:15:39 +0200

    Initial commit: Angular 9 project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kwa-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'kwa-app';
+}
