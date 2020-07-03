68651f299dd895788b89e09991f4ab4443fb2c07
Matthias Sprünglin <matthias.spruenglin@unibas.ch>
2020-07-03 10:38:34 +0200

    changed project code to correct value

--- knora/load-data.sh
+++ knora/load-data.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 for filename in kwa-data/import/segment*.xml; do
-    curl -v -u matthias.spruenglin@unibas.ch:Schaubuehne --data @"$filename" --header "Content-Type: application/xml" http://localhost:3333/v1/resources/xmlimport/http%3A%2F%2Frdfh.ch%2Fprojects%2FCAFE
+    curl -v -u kwa-admin@example.com:Schaubuehne --data @"$filename" --header "Content-Type: application/xml" http://localhost:3333/v1/resources/xmlimport/http%3A%2F%2Frdfh.ch%2Fprojects%2F0829
 done
