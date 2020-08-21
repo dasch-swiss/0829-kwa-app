1be91d2732676b3ec85bb13c03b76dd78ed2027c
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2020-08-21 10:19:20 +0200

    param update for saving searches

--- src/app/search-module/search-root/search-root.component.ts
+++ src/app/search-module/search-root/search-root.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {GravsearchServiceService} from "../../services/gravsearch-service.service";
 import { Queries } from '../../queries/queries';
 import { map } from 'rxjs/operators';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'kwa-search-root',
@@ -42,11 +43,26 @@ export class SearchRootComponent implements OnInit {
     queries: any = (new Queries).queries;
 
   constructor(
-      private gravsearchServiceService: GravsearchServiceService
+      private gravsearchServiceService: GravsearchServiceService,
+      private router: Router,
+      private route: ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
-      console.log( 'initiate search' );
+      // console.log( this.route.snapshot.queryParams );
+      for ( let param in this.route.snapshot.queryParams ) {
+          if ( typeof +param === 'number' ) {
+              this.chosenFilters.push( JSON.parse( this.route.snapshot.queryParams[ param ] ) );
+          }
+      }
+      console.log( this.chosenFilters );
+      for ( let i = 0; i < this.chosenFilters.length; i++ ) {
+          this.filterRows[ i ] = {
+              searchTerm: this.chosenFilters[ i ].searchTerm,
+              filter: this.chosenFilters[ i ].filter,
+              displayed: this.chosenFilters[ i ].operator
+          };
+      }
       this.gravsearchServiceService.sendGravsearchRequest( this.queries.noFilter.body )
           .pipe(
               map((response) => {
@@ -58,12 +74,12 @@ export class SearchRootComponent implements OnInit {
                           conceptId: entry['kwa:hasKwaConceptId']['knora-api:valueAsString'],
                           // expressions can be an array or an object or undefined!
                           // we want it to be an array in any case
-                          expressions: ( entry['knora-api:hasIncomingLinkValue'] ? 
-                          					( Array.isArray(entry['knora-api:hasIncomingLinkValue']) ? 
-                          					( entry['knora-api:hasIncomingLinkValue'] as any).map(expression => {return this.mapExpression(expression);}) 
+                          expressions: ( entry['knora-api:hasIncomingLinkValue'] ?
+                          					( Array.isArray(entry['knora-api:hasIncomingLinkValue']) ?
+                          					( entry['knora-api:hasIncomingLinkValue'] as any).map(expression => {return this.mapExpression(expression);})
                           								: [this.mapExpression(entry['knora-api:hasIncomingLinkValue'])])
                           								: [])
-                          }})}))                                                
+                          }})}))
           .subscribe(
               transformedEntries => {
                   console.log( transformedEntries );
@@ -71,31 +87,43 @@ export class SearchRootComponent implements OnInit {
               }, error => console.log( error )
           )
   }
-  
-  
+
+
    // maps a text expression.
    mapExpression( expression: any) : any {
-   	   return {   	   
+   	   return {
 				title: expression['knora-api:linkValueHasSource']['kwa:hasTitle']['knora-api:valueAsString'],
 				incipit: expression['knora-api:linkValueHasSource']['kwa:hasIncipit']['knora-api:valueAsString'],
 				// here comes the fun part
-				textcarrier: this.mapTextcarrier( expression['knora-api:linkValueHasSource']['kwa:standoffResourceTextResourceReferenceValue']['knora-api:linkValueHasTarget']['knora-api:hasIncomingLinkValue']['knora-api:linkValueHasSource']['kwa:onSurfaceValue']['knora-api:linkValueHasTarget']['kwa:partOfTextcarrierValue']['knora-api:linkValueHasTarget'])				
+				textcarrier: this.mapTextcarrier( expression['knora-api:linkValueHasSource']['kwa:standoffResourceTextResourceReferenceValue']['knora-api:linkValueHasTarget']['knora-api:hasIncomingLinkValue']['knora-api:linkValueHasSource']['kwa:onSurfaceValue']['knora-api:linkValueHasTarget']['kwa:partOfTextcarrierValue']['knora-api:linkValueHasTarget'])
 			}
 		}
-   	   
-		
+
+
 	mapTextcarrier ( textcarrier: any) : any {
 		return {
 				imprintedDate : textcarrier['kwa:hasImprintedDate']['knora-api:valueAsString'],
 				title : textcarrier['kwa:hasTitle']['knora-api:valueAsString'],
-				number : textcarrier['kwa:hasNumber']['knora-api:valueAsString']				
+				number : textcarrier['kwa:hasNumber']['knora-api:valueAsString']
 		}
-	}	
+	}
 
     updateDefinedFilterArray( key: string, value: string, index: number ) {
       this.chosenFilters[ index ] = this.chosenFilters[ index ] ? this.chosenFilters[ index ] : {};
+      console.log( value );
       this.chosenFilters[ index ][ key ] = value;
       console.log( this.chosenFilters );
+      this.updateUrlParams();
+    }
+
+    updateUrlParams() {
+        let stringyfiedArray = [];
+        for ( let filter of this.chosenFilters ) {
+            stringyfiedArray.push( JSON.stringify( filter ) );
+        }
+        this.router.navigate( [], {
+            queryParams: stringyfiedArray
+        } );
     }
 
 }
